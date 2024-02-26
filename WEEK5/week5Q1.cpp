e68fb034c837ccf04843447574075bf43d6910d6
Devansh-havingFOMO <152007728+Devansh-havingFOMO@users.noreply.github.com>
2024-02-26 09:46:21 +0530

    Create week5Q1.cpp

---
+++ WEEK5/week5Q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+vector<int> countSort(vector<int>& inputArray)
+{
+  int max=0,i=0,j=0;
+}
+int main()
+{
+  int test,n,res;
+  cout<<"Enter no of test cases:";
+  cin>>test;
+  for(int i=0;i<test;i++)
+    cout<<"Enter no of elements:";
+    cin>>n;
+    cout<<"Enter elements:";
+    for(int j=0;j<n;j++)
+    
