058b8615aa70c08c1144b1c74d19127a9ed84e1c
Devansh-havingFOMO <152007728+Devansh-havingFOMO@users.noreply.github.com>
2024-03-01 09:08:18 +0530

    Update week5Q1.cpp

--- WEEK5/week5Q1.cpp
+++ WEEK5/week5Q1.cpp
@@ -1,17 +1,48 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-vector<int> countSort(vector<int>& inputArray)
-{
-  int max=0,i=0,j=0;
+
+void CheckDuplicate(vector<char> arr){
+    int minEle=arr[0], maxEle=arr[0], len = arr.size();
+    for(int i=1; i<len; i++){
+        if(maxEle<arr[i])
+            maxEle = arr[i];
+        if(minEle>arr[i])
+            minEle = arr[i];
+    }
+    vector<int> countArr;
+    countArr.assign(maxEle-minEle+1, 0);
+    for(int i=0; i<len; i++){
+        countArr[arr[i]-minEle]++;
+    }
+    int maxInd=0, maxVal=countArr[0];
+    for(int i=1; i<countArr.size(); i++){
+        if(maxVal < countArr[i]){
+            maxInd = i;
+            maxVal = countArr[i];
+        }
+    }
+    if(maxVal>1){
+        cout<<char(maxInd+minEle)<<" - "<<maxVal<<endl;
+    } else {
+        cout<<"No Duplicates Present"<<endl;
+    }
 }
-int main()
-{
-  int test,n,res;
-  cout<<"Enter no of test cases:";
-  cin>>test;
-  for(int i=0;i<test;i++)
-    cout<<"Enter no of elements:";
-    cin>>n;
-    cout<<"Enter elements:";
-    for(int j=0;j<n;j++)
-    
+
+int main(){
+    int iter, len, res;
+    char temp;
+    cout<<"Enter number of cases: ";
+    cin>>iter;
+    for(int i=0; i<iter; i++){
+        cout<<"Enter length of "<<i+1<<" case: ";
+        cin>>len;
+        cout<<"Enter elements: ";
+        vector<char> arr;
+        for(int j=0; j<len; j++){
+            cin>>temp;
+            arr.push_back(temp);
+        }
+        CheckDuplicate(arr);
+    }
+}
